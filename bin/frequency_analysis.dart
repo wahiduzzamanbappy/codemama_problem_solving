2f8cfd2ca98cab05b8e2d8a6cc742d8d6ac47c94
Md Wahiduzzaman Bappy <wahidbappy253@gmail.com>
2025-03-02 21:12:36 +0600

    new problem m-22 solve.

--- bin/frequency_analysis.dart
+++ bin/frequency_analysis.dart
@@ -1,28 +1,11 @@
 import 'dart:io';
 
 void main() {
-  int N = int.parse(stdin.readLineSync()!.trim());
-  List<int> numbers =
-  stdin.readLineSync()!.trim().split(' ').map(int.parse).toList();
+  List<int> numbers = stdin.readLineSync()!.split(' ').map(int.parse).toList();
 
-  // Count frequency of each number while maintaining order
-  Map<int, int> freqCounter = {};
-  for (int num in numbers) {
-    freqCounter[num] = (freqCounter[num] ?? 0) + 1;
-  }
+  Iterable<int> evenNumbers = numbers.where((num) => num % 2 == 0);
 
-  // Sort based on frequency, then order of appearance
-  List<MapEntry<int, int>> sortedItems = freqCounter.entries.toList()
-    ..sort((a, b) => a.value == b.value
-        ? numbers.indexOf(a.key).compareTo(numbers.indexOf(b.key))
-        : a.value.compareTo(b.value));
 
-  // Output results
-  print(freqCounter.length);
-  for (var entry in sortedItems) {
-    print('${entry.key} ${entry.value}');
-  }
+  print(evenNumbers.length);
 }
-
-
 
